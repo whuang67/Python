2232f653c1831c252783f74f387875a3948e1bae
Wenke Huang <whuang67@illinois.edu>
2017-07-11 16:39:01 -0500

    Update Practice_Class_Def.py

--- miscellany/Practice_Class_Def.py
+++ miscellany/Practice_Class_Def.py
@@ -381,3 +381,63 @@ print tree.search(6)
 # Test print_tree
 # Should be 1-2-4-5-3
 print tree.print_tree()
+
+
+
+
+
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        self.BST_insert(self.root, new_val)
+    
+    def BST_insert(self, start, new_val):
+        if start.value < new_val:
+            if start.right:
+                self.BST_insert(start.right, new_val)
+            else:
+                start.right = Node(new_val)
+        elif start.value > new_val:
+            if start.left:
+                self.BST_insert(start.left, new_val)
+            else:
+                start.left = Node(new_val)
+        
+    def search(self, find_val):
+        return self.BST_search(self.root, find_val)
+    
+    def BST_search(self, start, find_val):
+        if start:
+            if start.value == find_val:
+                return True
+            elif start.value < find_val:
+                return self.BST_search(start.right, find_val)
+            else:
+                return self.BST_search(start.left, find_val)
+        return False
+    
+
+# Set up tree
+tree = BST(4)
+
+# Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+# Check search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
+print tree.search(5)

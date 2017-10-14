e0b825489baec86c783a78b70368ff8ba7f06624
Wenke Huang <whuang67@illinois.edu>
2017-10-14 14:56:23 -0500

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -31,9 +31,14 @@ class LogisticRegression():
         
         for _ in range(self.step):
             scores = np.matmul(self.X, self.w)
-            y_pred = sigmoid(scores)
-            self.w += self.learning_rate * np.matmul(self.X.T, self.y-y_pred)
-
+            y_pred = np.where(sigmoid(scores) > 0.5,
+                              np.ones(self.X.shape[0]),
+                              np.zeros(self.X.shape[0]))
+            
+            exp = np.exp(-np.dot(self.y, y_pred))
+            gradient = exp/(1+exp) * np.matmul(self.X.T, self.y)
+            # self.w += self.learning_rate * np.matmul(self.X.T, self.y-y_pred)
+            self.w += self.learning_rate * gradient
         # return self.w
     
     def predict(self, test_data = None, output = "label", cutoff = .5):
@@ -44,8 +49,8 @@ class LogisticRegression():
             label = sigmoid(np.matmul(test_data, self.w))
             if output == "label":
                 return np.where(label > cutoff,
-                                np.ones(self.X.shape[0]),
-                                np.zeros(self.X.shape[0]))
+                                np.ones(test_data.shape[0]),
+                                np.zeros(test_data.shape[0]))
             elif output == "probability":
                 return label
             else:
